9037266eadb032ce9a06ede65257c417153e805f
miinscar <128962867+miinscar@users.noreply.github.com>
2024-09-27 11:06:44 +0900

    Title: 변수 값 교체하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240927/변수 값 교체하기 3/replacing-variable-values-3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 3, b = 5;
+    int temp;
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    printf("%d\n", a);
+    printf("%d", b);
+    return 0;
+    
+}
